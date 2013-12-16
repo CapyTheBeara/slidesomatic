a50529c77f606eb8d407f6322cd6d3c5b0ca83d9
monocle <hchw29@gmail.com>
2013-12-16 11:14:38 -0800

    Slideshare - get rid of bouncing arrow

--- app/components/deck-controls/slideshare.js
+++ app/components/deck-controls/slideshare.js
@@ -1,13 +1,5 @@
 export default Ember.Mixin.create({
   updatePlayerSlide: function() {
-    var slide = this.get('slide'),
-        deckView = this.get('deckView');
-
-    deckView.jumpTo(slide);
-
-    if (slide === 1) {  // get rid of bouncing arrow
-      deckView.next();
-      deckView.previous();
-    }
+    this.get('deckView').jumpTo(this.get('slide'));
   }
 });
--- app/components/slidedeck-deck.js
+++ app/components/slidedeck-deck.js
@@ -1,5 +1,20 @@
 var endpoint = "http://static.slidesharecdn.com/swf/ssplayer2.swf";
 
+// http://learnswfobject.com/advanced-topics/executing-javascript-when-the-swf-has-finished-loading/
+function swfLoadEvent(fn, e){
+  if (typeof fn !== "function") { return false; }
+  var initialTimeout = window.setTimeout(function (){
+    if(typeof e.ref.PercentLoaded !== "undefined" && e.ref.PercentLoaded()){
+      var loadCheckInterval = window.setInterval(function (){
+        if(e.ref.PercentLoaded() === 100){
+          fn();
+          window.clearInterval(loadCheckInterval);
+        }
+      }, 1500);
+    }
+  }, 200);
+}
+
 export default Ember.Component.extend({
   elementId: "slide-player",
   docId: null,
@@ -12,9 +27,13 @@ export default Ember.Component.extend({
         atts = { id: id },
         flashvars = { doc : this.get('docId'), startSlide : 1, rel : 0 };
 
-    swfobject.embedSWF(endpoint, id, "100%", "100%", "8", null, flashvars, params, atts, function() {
-      self.set('deckView', document.getElementById(id));
+    swfobject.embedSWF(endpoint, id, "100%", "100%", "8", null, flashvars, params, atts, function(e) {
+      swfLoadEvent(function() {
+        var view = document.getElementById(id);
+        view.next(); // get rid of bouncing arrow
+        view.previous();
+        self.set('deckView', view);
+      }, e);
     });
-
   }
 });
