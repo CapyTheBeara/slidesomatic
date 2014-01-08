1af86990deafe4b438c5ddf349ecdf7bbcdfe110
monocle <hchw29@gmail.com>
2014-01-08 12:17:26 -0800

    Make input-slider work for Firefox

--- app/components/input-slider.js
+++ app/components/input-slider.js
@@ -9,11 +9,23 @@ export default Ember.Component.extend({
   value: 50,
   magnitude: 1,
 
+  // Chrome fires this when arrowing/sliding.
+  // Firefox only fires this when releasing slider or
+  // tabbing away
   change: function(evt) {
+    this.input(evt);
+  },
+
+  // Chrome fires this when sliding (not arrowing).
+  // Firefox fires this when arrowing/sliding.
+  input: function(evt) {
     var value = this.get('value'),
-        newValue = evt.target.value;
+        newValue = evt.target.value,
+        change = newValue - value;
+
+    if (change === 0) { return; }
 
     this.set('value', newValue);
-    this.sendAction('action', this.get('magnitude')*(newValue - value));
+    this.sendAction('action', this.get('magnitude') * change);
   }
 });
--- app/routes/index.js
+++
@@ -1,5 +0,0 @@
-export default Ember.Route.extend({
-  setupController: function() {
-    this.controllerFor('application').set('wrapperClass', 'index');
-  }
-});
